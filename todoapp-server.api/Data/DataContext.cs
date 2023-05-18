04428c1d0805a0e8f2735e923fd2567e109a5ce6
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-18 03:53:29 +0300

    Add DataContext & InitialMigration

---
+++ todoapp-server.api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todoapp_server.api.Models;
+
+namespace todoapp_server.api.Data;
+
+public class DataContext : DbContext
+{
+	public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+	public DbSet<User> Users { get; set; }
+}
