06cbe277d42246a995a0093ce0c98874f9b0f334
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-18 03:58:45 +0300

    Add User Model

---
+++ todoapp-server.Library/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace todoapp_server.Library.Models;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+public class User
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+	public string Email { get; set; } = string.Empty;
+	public byte[] PasswordHash { get; set; }
+	public byte[] PasswordSalt { get; set; }
+}
