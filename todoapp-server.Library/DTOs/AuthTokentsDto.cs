b78fa10c471b6ec4730c2ff6d9ac96f5f1dd612c
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-18 03:58:36 +0300

    Add DTOs for Login, Register & AuthTokens

---
+++ todoapp-server.Library/DTOs/AuthTokentsDto.cs
@@ -0,0 +1,7 @@
+﻿namespace todoapp_server.Library.DTOs;
+
+public class AuthTokentsDto
+{
+	public string RefreshToken { get; set; } = string.Empty;
+	public string AccessToken { get; set; } = string.Empty;
+}
---
+++ todoapp-server.Library/DTOs/RegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todoapp_server.Library.DTOs;
+public class RegisterDto
+{
+	public string Email { get; set; } = string.Empty;
+	public string Name { get; set; } = string.Empty;
+	public string Password { get; set; } = string.Empty; 
+}
