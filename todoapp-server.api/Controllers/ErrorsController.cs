a813fc7daced761d1c6c00005f5e6ede41054fc6
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-18 03:54:25 +0300

    Add ErrorsController

---
+++ todoapp-server.api/Controllers/ErrorsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace todoapp_server.api.Controllers;
+
+
+public class ErrorsController : ControllerBase
+{
+	[Route("/error")]
+	public IActionResult Error()
+	{
+		return Problem();
+	}
+}
