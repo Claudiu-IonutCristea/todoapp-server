709bfe2b401a9e39f6966f2c51f577430b72d075
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-18 03:56:47 +0300

    Services, ErrorHandler & SwaggerAuthentification

--- todoapp-server.api/Program.cs
+++ todoapp-server.api/Program.cs
@@ -1,11 +1,31 @@
+global using todoapp_server.api.Data;
+global using todoapp_server.Library.ServiceErrors;
+global using ErrorOr;
+
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using todoapp_server.api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+	options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+	{
+		Description = "Standard Authgiorization header using the Beared scheme (\"bearer {token}\")",
+		In = ParameterLocation.Header,
+		Name = "Authorization",
+		Type = SecuritySchemeType.ApiKey,
+	});
+
+	options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -15,8 +35,15 @@ if(app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
