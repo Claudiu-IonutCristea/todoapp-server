2030c5e1594e463c46511d3e2e28d9c9be31f771
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-18 03:59:05 +0300

    Add Service Errors

---
+++ todoapp-server.Library/ServiceErrors/Errors.Authentification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErrorOr;
+
+namespace todoapp_server.Library.ServiceErrors;
+
+public static partial class Errors
+{
+	public static class Login
+	{
+		public static Error EmailNotFound => Error.Validation(
+			code: "auth.email.notFound");
+		public static Error PasswordIncorect => Error.Validation(
+			code: "auth.password.incorrect");
+	}
+
+	public static class Register
+	{
+		public static Error EmailAlreadyInUse => Error.Validation(
+			code: "auth.email.alreadyExists",
+			description: "An account with this email already exists!");
+	}
+}
