bf3828831c282e42d6fb189c07f7865e6fed5e1a
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-18 03:54:16 +0300

    Add AuthentificationController

---
+++ todoapp-server.api/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using todoapp_server.Library.DTOs;
+
+namespace todoapp_server.api.Controllers;
+
+public class AuthController : ApiController
+{
+	[HttpPost("login")]
+	public async Task<ActionResult<AuthTokentsDto>> Login(LoginDto login)
+	{
+
+
+		return Ok();
+	}
+}
