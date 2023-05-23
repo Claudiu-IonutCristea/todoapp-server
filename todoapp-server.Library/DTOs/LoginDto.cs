d5fe43d364e486cffd18fd25145b6bc1f53505c2
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-24 01:13:07 +0300

    Required Attribute

--- todoapp-server.Library/DTOs/LoginDto.cs
+++ todoapp-server.Library/DTOs/LoginDto.cs
@@ -1,7 +1,11 @@
-﻿namespace ToDoAppServer.Library.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAppServer.Library.DTOs;
 
 public class LoginDto
 {
+	[Required]
 	public string Email { get; set; } = string.Empty;
+	[Required]
 	public string Password { get; set; } = string.Empty;
 }
--- todoapp-server.Library/DTOs/RegisterDto.cs
+++ todoapp-server.Library/DTOs/RegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,12 @@ using System.Threading.Tasks;
 namespace ToDoAppServer.Library.DTOs;
 public class RegisterDto
 {
+	[Required]
 	public string Email { get; set; } = string.Empty;
+
+	[Required]
 	public string Name { get; set; } = string.Empty;
-	public string Password { get; set; } = string.Empty; 
+
+	[Required]
+	public string Password { get; set; } = string.Empty;
 }
