ae624bb7dca256aa6ef9b25a2f93de86ea9048bc
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-24 01:15:18 +0300

    Dependency Injection - DataContext (InMemoryDatabase) -AuthentificationService -TokenService

---
+++ todoapp-server.Tests/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoAppServer.API.Data;
+using ToDoAppServer.API.Services;
+
+namespace ToDoAppServer.Tests;
+
+//https://github.com/pengweiqhca/Xunit.DependencyInjection
+
+public class Startup
+{
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddDbContext<DataContext>(options =>
+		{
+			options.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString());
+		});
+
+		services.AddTransient<IAuthentificationService, AuthentificationService>();
+		services.AddTransient<ITokenService, TokenService>();
+	}
+}
