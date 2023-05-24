57ec2f90506c5ba8c7eadf615b7f336c5b44517b
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-25 00:12:30 +0300

    Add Erros.Token.cs

---
+++ todoapp-server.Library/ServiceErrors/Errors.Token.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAppServer.Library.ServiceErrors;
+public static partial class Errors
+{
+	public static class AccessToken
+	{
+		public static Error TokenKeyInvalid => Error.Unexpected(
+			code: "accesstoken.key.invalid",
+			description: "Access token key is invalid!");
+	}
+}
