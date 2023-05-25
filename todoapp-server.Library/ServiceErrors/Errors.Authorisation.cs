af5e83b20691d637235941801465afe54cd60c77
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-25 03:26:58 +0300

    Add Errors.Authorisation.cs (For JWT Bearer Error Handling)

---
+++ todoapp-server.Library/ServiceErrors/Errors.Authorisation.cs
@@ -0,0 +1,19 @@
+﻿using ErrorOr;
+
+namespace ToDoAppServer.Library.ServiceErrors;
+public static partial class Errors
+{
+	public static class Authorisation
+	{
+		public static Error TokenExpired => Error.Validation(
+			code: "auth.acctoken.expired",
+			description: "The provided access token is expired!");
+
+		public static Error NoAuthHeader => Error.NotFound(
+			code: "auth.acctoken.noauthheader",
+			description: "The request has no authorisation header!");
+
+		public static Error Unexpected => Error.Unexpected(
+			code: "auth.acctoken.unexpected");
+	}
+}
