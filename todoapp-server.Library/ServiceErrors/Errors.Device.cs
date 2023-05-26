b96884680f10e78e8a3d89cf55f2dfe7c414ebaf
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-26 04:36:08 +0300

    Add Errors.Device

---
+++ todoapp-server.Library/ServiceErrors/Errors.Device.cs
@@ -0,0 +1,24 @@
+﻿using ErrorOr;
+
+namespace ToDoAppServer.Library.ServiceErrors;
+
+public static partial class Errors
+{
+	public static class Device
+	{
+		public static Error NoMatchUserAgentFamily => Error.Validation(
+			code: "device.uafamily.nomatch");
+
+		public static Error NoMatchDeviceFamily => Error.Validation(
+			code: "device.devicefamily.nomatch");
+
+		public static Error NoMatchOSFamily => Error.Validation(
+			code: "device.osfamily.nomatch");
+
+		public static Error NoMatchIpAddress => Error.Validation(
+			code: "device.ipaddress.nomatch");
+
+		public static Error Success => Error.Validation(
+			code: "device.validation.success");
+	}
+}
