a1fd6c4e92a30225f0390971fa0a5b7b90ba0fc7
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-05-26 04:27:07 +0300

    Add DeviceDto

---
+++ todoapp-server.Library/DTOs/DeviceDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAppServer.Library.DTOs;
+
+public class DeviceDto
+{
+	[MaxLength(16)] //16 bytes for IPv6 (future proofing here oK?), 4 bytes for IPv4
+	public byte[]? IPAddress { get; set; }
+
+	[Required]
+	[MaxLength(100)]
+	public string UserAgentFamily { get; set; } = string.Empty;
+
+	[MaxLength(50)]
+	public string OSFamily { get; set; } = string.Empty;
+
+	[MaxLength(50)]
+	public string DeviceFamily { get; set; } = string.Empty;
+}
